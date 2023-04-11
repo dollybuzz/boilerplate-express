64fd39c50f1538823d93b2f15fd02e4bce02419a
Replit user <>
2023-04-11 21:06:50 +0000

    step 4 done, pass static file via express middleware

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + '/views/index.html';
+let assetPath = __dirname + '/public';
 
 console.log("Hello World");
 
+app.use("/public", express.static(assetPath));
+
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  //res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
+
 
 
 
