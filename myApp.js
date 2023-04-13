7450eb72af93c99f6f5c7df757aaf511e1732251
Replit user <>
2023-04-13 21:39:33 +0000

    step 7 done, add basic logger via middleware function

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ console.log("Hello World");
 
 app.use("/public", express.static(assetPath));
 
+app.use(function(req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 app.get("/", function(req, res) {
   //res.send("Hello Express");
   res.sendFile(absolutePath);
@@ -16,7 +21,8 @@ app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({ "message": "HELLO JSON" });
   } else { res.json({ "message": "Hello json" }); }
-
 });
+
+
 
 module.exports = app;
