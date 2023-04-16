e14ae352b19fc080fb2773e8fcbebfe774e11a17
Replit user <>
2023-04-16 12:31:37 +0000

    chain middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ console.log("Hello World");
 
 app.use("/public", express.static(assetPath));
 
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 app.get("/", function(req, res) {
   //res.send("Hello Express");
   res.sendFile(absolutePath);
@@ -16,7 +21,14 @@ app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({ "message": "HELLO JSON" });
   } else { res.json({ "message": "Hello json" }); }
-
 });
+
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({ time : req.time });
+});
+
 
 module.exports = app;
